7d22bbfd7c06eeead5af5aa549967ccf447b05fd
Mavil_Co-HP\Mavil_Co <rmavil@hotmail.com>
2017-06-14 11:43:22 -0500

    cambio de idioma en los botones guardar

--- resources/views/cat_juezs/fields.blade.php
+++ resources/views/cat_juezs/fields.blade.php
@@ -17,6 +17,6 @@
 </div>
 <!-- Submit Field -->
 <div class="form-group col-sm-12">
-    {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
+    {!! Form::submit('Guardar', ['class' => 'btn btn-primary']) !!}
     <a href="{!! route('catJuezs.index') !!}" class="btn btn-default">Cancel</a>
 </div>
