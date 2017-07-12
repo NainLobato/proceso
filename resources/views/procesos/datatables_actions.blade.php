eeaf0feab11dcc61aa7d614d835544fcf1c8d8a7
Tomas G.Serrano Escamilla <tserrano@veo.tv>
2017-07-12 17:40:50 -0500

    deshabilitando boton eliminar en el datatable de procesos

--- resources/views/procesos/datatables_actions.blade.php
+++ resources/views/procesos/datatables_actions.blade.php
@@ -6,10 +6,13 @@
     <a href="{{ route('procesos.edit', $id) }}" class='btn btn-default btn-xs'>
         <i class="glyphicon glyphicon-edit"></i>
     </a>
+<!-- if is superuser
+
     {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', [
         'type' => 'submit',
         'class' => 'btn btn-danger btn-xs',
         'onclick' => "return confirm('Estás seguro de confirmar?')"
     ]) !!}
+-->
 </div>
 {!! Form::close() !!}
