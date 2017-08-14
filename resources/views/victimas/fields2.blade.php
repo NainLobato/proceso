e6dbfdf68cd60ee1c691c997c568c48969c375a0
Nain Lobato <wolverinenain@gmail.com>
2017-08-14 11:17:23 -0500

    Mejoras en diseño responsivo en editar proceso

--- resources/views/victimas/fields2.blade.php
+++ resources/views/victimas/fields2.blade.php
@@ -8,59 +8,57 @@
     <div id="divVictimas" class="panel-collapse collapse in">
     <div class="box box-default">
     <div class="box-body">
-    <div class="row">
-        {!! Form::open(['id' => 'frmVictimas']) !!}
-        <div class="col-md-3 col-xs-12">
-             <div class="form-group">
-                {!! Form::label('idVictima', 'Nombre Victima:') !!}<br>
-                {!! Form::select('idVictima', array(), null, ['class' => 'form-control']) !!}
-            </div>
-        </div>
+        <div class="row">
+            {!! Form::open(['id' => 'frmVictimas']) !!}
+                <div class="col-xs-12 col-md-6">
+                     <div class="form-group">
+                        {!! Form::label('idVictima', 'Nombre Victima:') !!}<br>
+                        {!! Form::select('idVictima', array(), null, ['class' => 'form-control']) !!}
+                    </div>
+                </div>
 
-        <div class="col-md-3 col-xs-12">
-            <div class="form-group">
-                {!! Form::label('idDireccion', 'Direccion:') !!}<br>
-                {!! Form::select('idDireccionVictima', $direcciones, null, ['name'=>'idDireccionVictima','id'=>'idDireccionVictima','class' => 'form-control']) !!}
+                <div class="col-xs-12 col-md-6">
+                    <div class="form-group">
+                        {!! Form::label('idDireccion', 'Direccion:') !!}<br>
+                        {!! Form::select('idDireccionVictima', $direcciones, null, ['name'=>'idDireccionVictima','id'=>'idDireccionVictima','class' => 'form-control']) !!}
+                    </div>
+                </div>
+            {!! Form::close() !!}   
+            <div class="clearfix"></div>
+            <div class="col-xs-6 col-md-2">
+                <div class="form-group">
+                    <br>
+                    <button type="button" class="btn btn-primary add-proceso-victima">Agregar Victima</button>
+                </div>
+            </div>
+            <div class="col-xs-6 col-md-2">
+                <div class="form-group">
+                    <br>
+                    <button type="button" data-href="/procesos/public/personas/createModal" class="btn btn-primary modal-persona" data-target="#myModal" data-toggle="modal"> Crear Victima </button>
+                </div>
             </div>
         </div>
-        {!! Form::close() !!}   
-
-        <div class="col-md-3 col-xs-6">
-            <div class="form-group">
-                <br>
-                <button type="button" class="btn btn-primary add-proceso-victima">Agregar Victima</button>
-            </div>
-        </div>
-        
-        <div class="col-md-3 col-xs-6">
-            <div class="form-group">
-                <br>
-                <button type="button" data-href="/procesos/public/personas/createModal" class="btn btn-primary modal-persona" data-target="#myModal" data-toggle="modal"> Crear Victima </button>
-            </div>
-        </div>
-    </div>
     
         <div class="relation-proceso-victima">
             @if ($action == 'editar')
                 @foreach($selectedVictimas as $victima)
                     <div class="row row-proceso-victima" data-victima-id="{!! $victima->id !!}">
                         <input type="hidden" name="victimas[]" value="{!! $victima->id !!}">
-                        
-                        <div class="col-sm-10 col-xs-10">
-                        <div class="form-group"><i class="fa fa-user-circle-o fa-2x" aria-hidden="true"></i> 
-                            {!! $victima->nombre !!}
+                        <div class="col-xs-10">
+                            <div class="form-group"><i class="fa fa-user-circle-o fa-2x" aria-hidden="true"></i> 
+                                {!! $victima->nombre !!}
+                            </div>
                         </div>
-                        </div>
-                        <div class="col-sm-2 col-xs-2 text-center">
-                        <div class="form-group">
-                            <i class="fa fa-times icon-red remove-proceso-victima"></i>
-                        </div>
+                        <div class="col-xs-1 text-center">
+                            <div class="form-group">
+                                <i class="fa fa-times icon-red remove-proceso-victima"></i>
+                            </div>
                         </div>
                     </div>
                 @endforeach
             @endif
-</div>
-</div>
-</div>
-</div>
+        </div>
+    </div>
+    </div>
+    </div>
 </div>
